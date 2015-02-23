d252d3a0a74a0d460b6f81db7cb5353cad9f830c
andrea-manzi <andrea.manzi@gmail.com>
2015-02-23 11:20:00 +0100

    added tests

--- Modulefile
+++ Modulefile
@@ -1,5 +1,5 @@
 name    'lcgdm-dpm'
-version '0.1.0'
+version '0.1.1'
 source 'UNKNOWN'
 author 'lcgdm'
 license 'Apache License, Version 2.0'
--- tests/init.pp
+++ tests/init.pp
@@ -9,4 +9,16 @@
 # Learn more about module testing here:
 # http://docs.puppetlabs.com/guides/tests_smoke.html
 #
-include dpm
+
+class{"dpm::head_disknode":
+   configure_default_pool => true,
+   configure_default_filesystem => true,
+   disk_nodes => "localhost",
+   localdomain => "cern.ch",
+   db_pass => "MYSQLPASS",
+   mysql_root_pass => "PASS",
+   token_password => "TOKEN_PASSWORD",
+   xrootd_sharedkey => "A32TO64CHARACTERKEYTESTTESTTESTTEST",
+   site_name => "CNR_DPM_TEST",
+   volist =>[dteam],
+}
